795936f0a0b7780a92fdba05b427a5c3c6a8c513
pc <beata.jackowska99@gmail.com>
2020-11-15 14:43:07 +0100

    initial animation

---
+++ Template1/Template1.cpp
@@ -0,0 +1,160 @@
+﻿// Template1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+#include "include\GL\GL.H"
+#include "include\GL\GLU.H"
+#include "include\GL\glut.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+#define GL_GLEXT_PROTOTYPES
+
+
+// ----------------------------------------------------------
+// Function Prototypes
+// ----------------------------------------------------------
+void display();
+void specialKeys();
+
+// ----------------------------------------------------------
+// Global Variables
+// ----------------------------------------------------------
+double rotate_y = 0;
+double rotate_x = 0;
+
+// ----------------------------------------------------------
+// display() Callback function
+// ----------------------------------------------------------
+void MyDisplay() {
+
+    //  Clear screen and Z-buffer
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Reset transformations
+    glLoadIdentity();
+
+    // Other Transformations
+    // glTranslatef( 0.1, 0.0, 0.0 );      // Not included
+    // glRotatef( 180, 0.0, 1.0, 0.0 );    // Not included
+
+    // Rotate when user changes rotate_x and rotate_y
+    glRotated(rotate_x, 1.0, 0.0, 0.0);
+    glRotated(rotate_y, 0.0, 1.0, 0.0);
+
+    // Other Transformations
+    // glScalef( 2.0, 2.0, 0.0 );          // Not included
+
+    //Yellow side - FRONT
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 0.78, 0.0);     
+    glVertex3f(0.5, -0.5, -0.5);      // P1 is red
+    glVertex3f(0.5, 0.5, -0.5);      
+    glVertex3f(-0.5, 0.5, -0.5);      
+    glVertex3f(-0.5, -0.5, -0.5);      
+    glEnd();
+
+    // White side - BACK
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 1.0, 1.0);
+    glVertex3f(0.5, -0.5, 0.5);
+    glVertex3f(0.5, 0.5, 0.5);
+    glVertex3f(-0.5, 0.5, 0.5);
+    glVertex3f(-0.5, -0.5, 0.5);
+    glEnd();
+
+    // Purple side - RIGHT
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 0.0, 1.0);
+    glVertex3f(0.5, -0.5, -0.5);
+    glVertex3f(0.5, 0.5, -0.5);
+    glVertex3f(0.5, 0.5, 0.5);
+    glVertex3f(0.5, -0.5, 0.5);
+    glEnd();
+
+    // Green side - LEFT
+    glBegin(GL_POLYGON);
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(-0.5, -0.5, 0.5);
+    glVertex3f(-0.5, 0.5, 0.5);
+    glVertex3f(-0.5, 0.5, -0.5);
+    glVertex3f(-0.5, -0.5, -0.5);
+    glEnd();
+
+    // Blue side - TOP
+    glBegin(GL_POLYGON);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex3f(0.5, 0.5, 0.5);
+    glVertex3f(0.5, 0.5, -0.5);
+    glVertex3f(-0.5, 0.5, -0.5);
+    glVertex3f(-0.5, 0.5, 0.5);
+    glEnd();
+
+    // Red side - BOTTOM
+    glBegin(GL_POLYGON);
+    glColor3f(1.0, 0.0, 0.0);
+    glVertex3f(0.5, -0.5, -0.5);
+    glVertex3f(0.5, -0.5, 0.5);
+    glVertex3f(-0.5, -0.5, 0.5);
+    glVertex3f(-0.5, -0.5, -0.5);
+    glEnd();
+
+    glFlush();
+    //glutSwapBuffers(); //po chuja
+}
+
+// ----------------------------------------------------------
+// specialKeys() Callback Function
+// ----------------------------------------------------------
+void specialKeys(int key, int x, int y) {
+
+    //  Right arrow - increase rotation by 5 degree
+    if (key == GLUT_KEY_RIGHT)
+        rotate_y += 5;
+
+    //  Left arrow - decrease rotation by 5 degree
+    else if (key == GLUT_KEY_LEFT)
+        rotate_y -= 5;
+
+    else if (key == GLUT_KEY_UP)
+        rotate_x += 5;
+
+    else if (key == GLUT_KEY_DOWN)
+        rotate_x -= 5;
+
+    //  Request display update
+    glutPostRedisplay();
+
+}
+
+
+// ----------------------------------------------------------
+// main() function
+// ----------------------------------------------------------
+int main(int argc, char* argv[]) {
+
+    //  Initialize GLUT and process user parameters
+    glutInit(&argc, argv);
+
+    //  Request double buffered true color window with Z-buff	
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB );
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition(100, 100);
+    // Create window
+    glutCreateWindow("Awesome Cube");
+
+    //  Enable Z-buffer depth test
+    glEnable(GL_DEPTH_TEST);
+    //MyInit();
+    // Callback functions
+    glutDisplayFunc(MyDisplay);
+    glutSpecialFunc(specialKeys);
+
+    //  Pass control to GLUT for events
+    glutMainLoop();
+
+    //  Return to OS
+    return 0;
+
+}
